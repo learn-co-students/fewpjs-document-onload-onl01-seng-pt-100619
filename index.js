4f94b6daadca3d5a2d08fb3aba2da767dd1c3634
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-05-02 07:53:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const pTag = document.getElementById('text');
+    pTag.innerHTML = "This is really cool!"
+  });
