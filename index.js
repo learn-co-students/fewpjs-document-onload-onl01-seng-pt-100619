7cfb1077c4dcf1c292e8172c3ad7be5693344aaf
steelerx155 <steelerx155@gmail.com>
2020-05-06 14:45:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+  });
+
+//   If you put the above code in index.js, 'The DOM has loaded' will not be logged immediately. 
+//   In fact, you can confirm this yourself by putting a second console.log() outside of the event listener callback:
+
+
+
+  function updateDOM() {
+      document.getElementById( "text")
+      .innerHTML = "This is really cool!";
+  }
