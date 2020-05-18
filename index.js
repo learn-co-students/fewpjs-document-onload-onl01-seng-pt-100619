467ad71dd3651bef51f3683df26b1bc7e7e3ab17
kelly_maass <kmaass77627@gmail.com>
2020-05-17 19:04:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    let element = document.querySelector("p#text")
+    element.innerHTML = "This is really cool!"
+    
+    console.log("The DOM has loaded");
+
+  });
+   
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
+
+ // Use the event's callback function to target the paragraph element with id="text" 
+ // and replace the text with "This is really cool!"
+ // textContent
+
+  
